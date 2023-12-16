4faac64f8e568789af0b390c24785dca5f044031
sejal-sali <sejalsali070402@gmail.com>
2023-12-16 06:10:56 +0530

    packages and app update

--- bus_web_app/src/App.js
+++ bus_web_app/src/App.js
@@ -1,44 +1,10 @@
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//     </div>
-//   );
-// }
-
-// export default App;
-// App.js
-// import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-// import Buses from './components/Buses';
-// import Busseats from './components/Busseats'; // Import the BusSeats component
-// import Home from './components/Home';
-
-// const App = () => {
-//   const handleCheckBuses = (fromCity, toCity) => {
-//     // Logic to handle checking buses with selected cities
-//     console.log(`From: ${fromCity}, To: ${toCity}`);
-//   };
-
-//   return (
-//     <Router>
-//       <Switch>
-//         <Route exact path="/">
-//           <Home handleCheckBuses={handleCheckBuses} />
-//         </Route>
-//         <Route path="/buses" component={Buses} />
-//         <Route path="/seats/:busId" component={Busseats} /> {/* New Route for BusSeats */}
-
-//       </Switch>
-//     </Router>
-//   );
-// };
-
-// export default App;
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Buses from './components/Buses';
-import Busseats from './components/Busseats'; // Updated import for BusSeats
+import Busseats from './components/Busseats';
 import Home from './components/Home';
+import login from './components/login'; // Updated import with correct casing
+import signup from './components/signup';
 
 const App = () => {
   const handleCheckBuses = (fromCity, toCity) => {
@@ -50,14 +16,18 @@ const App = () => {
     <Router>
       <Switch>
         <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route exact path="/login" component={login} />
+        <Route path="/signup" component={signup} />
+        <Route path="/home">
           <Home handleCheckBuses={handleCheckBuses} />
         </Route>
-        <Route exact path="/buses" component={Buses} /> {/* Updated path for Buses */}
-        <Route path="/seats/:busId" component={Busseats} /> {/* Updated component for BusSeats */}
+        <Route exact path="/buses" component={Buses} />
+        <Route path="/seats/:busId" component={Busseats} />
       </Switch>
     </Router>
   );
 };
 
 export default App;
-
