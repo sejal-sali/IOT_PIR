425eb4a1f4feb6885999d6959efeb68f96bea9e4
sejal-sali <sejalsali070402@gmail.com>
2023-12-16 09:18:07 +0530

    logo and ui changes

--- bus_web_app/src/components/Buses.js
+++ bus_web_app/src/components/Buses.js
@@ -1,7 +1,8 @@
 import { Button, Card, CardContent, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-import logo from '../assets/logo.svg'; // Import the SVG file
+import logo from '../assets/logo.png'; // Import the SVG file
+import './Home.css';
 
 const Buses = () => {
   const location = useLocation();
@@ -9,13 +10,13 @@ const Buses = () => {
   const [filteredBuses, setFilteredBuses] = useState([]);
 
   const busDetails = [
-    { id: 1, source: 'Dublin', destination: 'Dundalk', time: '9:00 AM', price: '50', travelName: 'Travel A' },
-    { id: 2, source: 'Dundalk', destination: 'Dublin', time: '10:30 AM', price: '55', travelName: 'Travel B' },
-    { id: 3, source: 'Drogheda', destination: 'Dublin', time: '11:00 AM', price: '60', travelName: 'Travel C' },
-    { id: 4, source: 'Dublin', destination: 'Drogheda', time: '1:00 PM', price: '65', travelName: 'Travel D' },
-    { id: 5, source: 'Drogheda', destination: 'Dundalk', time: '3:30 PM', price: '70', travelName: 'Travel E' },
-    { id: 6, source: 'Dundalk', destination: 'Drogheda', time: '5:00 PM', price: '75', travelName: 'Travel F' },
-    { id: 7, source: 'Dundalk', destination: 'Drogheda', time: '6:00 PM', price: '75', travelName: 'Travel G' },
+    { id: 1, source: 'Dublin', destination: 'Dundalk', time: '9:00 AM'},
+    { id: 2, source: 'Dundalk', destination: 'Dublin', time: '10:30 AM'},
+    { id: 3, source: 'Drogheda', destination: 'Dublin', time: '11:00 AM'},
+    { id: 4, source: 'Dublin', destination: 'Drogheda', time: '1:00 PM'},
+    { id: 5, source: 'Drogheda', destination: 'Dundalk', time: '3:30 PM'},
+    { id: 6, source: 'Dundalk', destination: 'Drogheda', time: '5:00 PM'},
+    { id: 7, source: 'Dundalk', destination: 'Drogheda', time: '6:00 PM'},
   ];
 
   useEffect(() => {
@@ -40,15 +41,20 @@ const Buses = () => {
     };
   }, [history]);
 
+  // const handleViewSeats = (busId) => {
+  //   console.log(`View seats for Bus ID: ${busId}`);
+  //   history.push(`/seats/${busId}`);
+  // };
   const handleViewSeats = (busId) => {
     console.log(`View seats for Bus ID: ${busId}`);
     history.push(`/seats/${busId}`);
   };
 
+
   return (
     <div>
       <div className="navbar">
-      <img src={logo} alt="Logo" width="200" height="50" />
+      <img src={logo} alt="Logo" className="logo" />
       </div>
 
       <div className="bus-cards-container">
@@ -58,7 +64,6 @@ const Buses = () => {
               <Typography variant="h6">{bus.travelName}</Typography>
               <Typography variant="subtitle1">{bus.source} to {bus.destination}</Typography>
               <Typography variant="body2">Time: {bus.time}</Typography>
-              <Typography variant="body2">Price: {bus.price} Euros</Typography>
               <Button variant="outlined" onClick={() => handleViewSeats(bus.id)}>View Seats</Button>
             </CardContent>
           </Card>
